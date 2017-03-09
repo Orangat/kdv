7127b177254c54b6ab4c27c11f6ee841603c21d7
Sergey Nazarov <dr.orangat@gmail.com>
2017-03-09 22:03:31 +0200

    so much fix

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
 		// autoplay: true,
 		autolaySpeed: 3000
 	});
-	
 });
 
 $(document).ready(function(){
@@ -17,7 +16,6 @@ $(document).ready(function(){
 	$('#ex5').zoom({ on:'click' });
 });
 
-
 $(document).ready(function(){
 	$('.map-dlv').click(function() {
 		$('.wrap-bable').slideDown('fast');
@@ -25,20 +23,51 @@ $(document).ready(function(){
 	$('.close-bbl').click(function() {
 		$('.wrap-bable').slideUp('fast');
 	});
-
 });
-
 
 $(document).ready(function() {
 	var elem = $(".zoom");
 	elem.on("click", function() {
-		if (elem.css("cursor") == "zoom-in") {
-			elem.css("cursor", "zoom-out");
+		if (elem.hasClass('zoom-plus')) {
+			elem.removeClass('zoom-plus');
+			elem.addClass('zoom-minus');
 		} else {
-			elem.css("cursor", "zoom-in");
+			elem.removeClass('zoom-minus');
+			elem.addClass('zoom-plus');
 		}
 	});
 })
+
+$(document).ready(function(){
+	$(".nav-input").focus(function(){
+		$(".tips-block").addClass("dsp-block").animate({opacity: 1}, 300);
+	}).blur(function(){
+		$(".tips-block").removeClass("dsp-block").animate({opacity: 0}, 300);
+	});
+});
+
+$(document).ready(function(){
+	$(".nav-input").focus(function(){
+		$(".nav-input").css('background', '#F8F8F8 url("../images/close-tips.png") no-repeat 98% center');
+
+	}).blur(function(){
+		$(".nav-input").css('background', '#F8F8F8 url("../images/search.png") no-repeat 98% center');
+	});
+});
+
+$(document).ready(function(){
+	var elem = $(".link-close");
+	elem.on("click", function() {
+		if ($('tips-block').css('display', 'none')) {
+			$('tips-block').css('display', 'block');
+		} else {
+			$('tips-block').css('display', 'block');
+		}
+	});
+});
+
+
+
 
 
 
